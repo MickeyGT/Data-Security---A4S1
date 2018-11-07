b4bea34f30008d26ad13dc932ffb100762389bb7
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2018-11-07 22:10:06 +0200

    Update CaesarCipher.cpp

--- CaesarCipher.cpp
+++ CaesarCipher.cpp
@@ -7,13 +7,16 @@ int val;
 int main()
 {
 	cout << "Sirul necryptat: ";
-	cin >> sirulNecriptat;
+	getline(cin, sirulNecriptat);
 	transform(sirulNecriptat.begin(), sirulNecriptat.end(), sirulNecriptat.begin(), ::tolower);
 	cout << "Valoarea cu care sa criptam sirul: ";
 	cin >> val;
 	string sirulCriptat(sirulNecriptat.length(), ' ');
 	for (int i = 0; i < sirulNecriptat.size(); i++)
-		sirulCriptat[i] = (sirulNecriptat[i] - 'a' + val) % 26+'a';
+		if (sirulNecriptat[i] != ' ')
+			sirulCriptat[i] = (sirulNecriptat[i] - 'a' + val) % 26 + 'a';
+		else
+			sirulCriptat[i] = ' ';
 	cout << "Sirul criptat este: "<<sirulCriptat;
 	return 0;
 }
